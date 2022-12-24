89ff35decc1ead9133ee0f746e109683e8fccd72
baukk <gaurtanay6@gmail.com>
2022-12-25 03:59:44 +0530

    Changed-Markers-and-added-colors-based-on-value

--- map1.py
+++ map1.py
@@ -7,18 +7,29 @@ data=pandas.read_csv("stateCoordinates.csv")
 name=list(data["name"])
 lat=list(data["lat"])
 long=list(data["long"])
+no=list(data["no"])
 
 html = '''<h4>%s</h4>
-<b>Latitude:</b>%s <b>Longitude:</b>%s'''
+<b>Latitude:</b>%s <b>Longitude:</b>%s
+<b>Value</b> : %s'''
 
+def colorproducer(value):
+    if value<33:
+         return "green"
+    elif value>32 and value<66:
+        return "orange"
+    else:
+        return "red"
 
-for i,j,k in zip(name,lat,long):
-    iframe = folium.IFrame(html=html %( str(i),str(j),str(k) ), width=200, height=100)
-    # iframe = folium.IFrame(html=html % str(el), width=200, height=100)
-    fg.add_child(folium.Marker(location=[j,k],popup=folium.Popup(iframe),icon=folium.Icon(color="blue")))
+htmlfortootlip=''' %s'''
+
+for i,j,k,l in zip(name,lat,long,no):
+    iframe = folium.IFrame(html=html %( str(i),str(j),str(k),str(l) ), width=200, height=100)
+    tt=htmlfortootlip%i
+    fg.add_child(folium.CircleMarker(location=[j,k],radius=4,popup=folium.Popup(iframe),fill=True,fill_color=colorproducer(l),color=colorproducer(l),fill_opacity=0.7, tooltip=tt))
 
 
 
 
 map.add_child(fg)
-map.save("map1.html")
+map.save("index.html")
